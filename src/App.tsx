3b3e50bd4c1621a8f891d686509baac23a8ac13b
Samuel Giraldo <ozupacademico@gmail.com>
2026-05-23 23:24:48 -0500

    connect frontend to backend collections API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+type Collection = {
+  id: number
+  name: string
+  type: string
+}
+
 function App() {
+
+  const [collections, setCollections] = useState<Collection[]>([])
+
+  useEffect(() => {
+
+    axios
+      .get("http://127.0.0.1:8000/collections/")
+      .then((response) => {
+
+        setCollections(response.data)
+
+      })
+      .catch((error) => {
+
+        console.error(error)
+
+      })
+
+  }, [])
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
+
       <h1>Magic Inventory App</h1>
+
+      <h2>Collections</h2>
+
+      {
+        collections.map((collection) => (
+
+          <div key={collection.id}>
+
+            <p>
+              {collection.name} ({collection.type})
+            </p>
+
+          </div>
+
+        ))
+      }
+
     </div>
   )
 }
